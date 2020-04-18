18d8b66b78b80ba1dc1da66913382d821ebe8f23
vfx-fuhao <vfx_fuhao@163.com>
2020-04-18 17:59:39 +0800

    add variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //declaring variables
+    int a,b;
+    int result;
+
+    //process
+    a = 5;
+    b = 2;
+    a += 1;
+    result = a- b;
+
+    //print out result;
+    cout<<result;
+    return 0;
+}
